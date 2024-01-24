789dc40db83fe7ca6aaf896460851b71a1d64d29
seongit <seongeunk102@gmail.com>
2024-01-24 16:36:10 +0900

    Initial Commit
    Signed-off-by: seongit <seongeunk102@gmail.com>

---
+++ src/main/java/com/dev/sonarqubeapi/SonarqubeApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.sonarqubeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/sonarqubeapi/SonarqubeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.sonarqubeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
