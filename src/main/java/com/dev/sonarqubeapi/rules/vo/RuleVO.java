daad70fddfb8a42e5e30e569d8c5221bbcb038bd
SeongEun Kim <80396754+seongit@users.noreply.github.com>
2024-01-26 21:55:23 +0900

    feat : add RuleVO File

---
+++ src/main/java/com/dev/sonarqubeapi/rules/vo/RuleVO.java
@@ -0,0 +1,13 @@
+package com.dev.sonarqubeapi.rules.vo;
+
+public class RuleVO {
+
+    public static class RuleIn{
+        private String page;
+    }
+    public static class RuleOut{
+
+    }
+
+
+}
