d99f8e781901c9cd2418a0a8306145ecb10ed858
SeongEun Kim <80396754+seongit@users.noreply.github.com>
2024-02-28 00:19:02 +0900

    SwaggerConfig 관련 임시주석
    Signed-off-by: SeongEun Kim <80396754+seongit@users.noreply.github.com>

--- src/main/java/com/dev/sonarqubeapi/config/SwaggerConfig.java
+++ src/main/java/com/dev/sonarqubeapi/config/SwaggerConfig.java
@@ -1,14 +1,12 @@
 package com.dev.sonarqubeapi.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(title = "SonarQube API 명세서"
-        , description = "소나큐브 API를 활용해서 사부작 사부작 이것 저것 만듭니다."
-        , version = "v1")
-)
+//@OpenAPIDefinition(
+//        info = @Info(title = "SonarQube API 명세서"
+//        , description = "소나큐브 API를 활용해서 사부작 사부작 이것 저것 만듭니다."
+//        , version = "v1")
+//)
 @Configuration
 public class SwaggerConfig {
 
